a4ff99d49005f92564f5db1fad3396c5951924e8
Landon Johnson <landon.johnson@utexas.edu>
2021-02-15 10:46:32 -0600

    Created the constructor which encodes a file

---
+++ HuffmanCoder.java
@@ -0,0 +1,9 @@
+import datastructs.*;
+import java.io.File;
+import java.util.Scanner;
+public class HuffmanCoder{
+    File txt;
+    Scanner scan;
+    PriorityQueue<special node> queue;
+    BinaryTree<special node> huffmanTree;
+}
