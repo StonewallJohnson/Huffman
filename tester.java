ef934e6b24c6fba53de176c08d69583a7bca25d0
Landon Johnson <landon.johnson@utexas.edu>
2021-02-18 13:30:09 -0600

    added testing material

---
+++ tester.java
@@ -0,0 +1,8 @@
+import java.io.File;
+public class tester {
+    public static void main(String[] args){
+        File give = new File("message.txt");
+        File recieve = new File("encoded.txt");
+        HuffmanCoder coder = new HuffmanCoder(give, recieve);
+    }
+}
